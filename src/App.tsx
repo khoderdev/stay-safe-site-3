cdd133e8d632a6983dc65ea84fd5b68bfa565d2a
oummaldev <oummal.dev@gmail.com>
2024-11-13 18:22:24 +0200

    styled scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { RouterProvider } from "react-router-dom";
 import router from './router';
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <RouterProvider router={router} />
   );
 }
+
 export default App;
