5173413fd05844e571be27a828a85dad06a22d15
oummaldev <oummal.dev@gmail.com>
2024-12-30 04:07:37 +0200

    implemented Fast React Mechanism

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,60 +1,42 @@
+import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../views/Home";
-import Posts from "../components/posts/Posts";
-import Single from "../components/posts/Single";
-import Write from "../components/posts/Write";
-import AboutUs from "../views/AboutUs";
+import { routes } from "../utils/routeConfig";
+import { usePrefetch, setCacheHeaders } from "../utils/optimization";
 
-import MDC from "../views/MDC";
-import OHS from "../views/OHS";
-import EnvirementalHealth from "../views/EnvirementalHealth";
+// Loading component for Suspense fallback
+const Loading = () => (
+  <div className="flex items-center justify-center min-h-screen">
+    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-pink"></div>
+  </div>
+);
 
-import DynamicDietPage from "../components/qualitative-diets/DynamicDietPage";
-import Users from "../views/Users";
-import QualitativeDiets from "../components/qualitative-diets/QualitativeDiets";
-import BookAppointment from "../components/forms/BookAppointment";
-import Vitrack from "../components/Vitrack/Vitrack";
+const OptimizedRoute = ({ route }) => {
+  const componentRef = usePrefetch(route);
 
-import PreventiveHealthPatientServices from "../views/PreventiveHealthPatientServices";
-import PublicHealthInterventions from "../views/PublicHealthInterventions";
-import PublicHealthAcademy from "../views/PublicHealthAcademy";
-
-import HealthAndSafety from "../views/HealthAndSafety";
-import Volunteering from "../components/Volunteering/Volunteering";
-import ShareAroundTheWorld from "../components/ShareAroundTheWorld/ShareAroundTheWorld";
-import Auth from "../views/Auth";
+  return (
+    <div ref={componentRef}>
+      <Suspense fallback={<Loading />}>
+        <route.component />
+      </Suspense>
+    </div>
+  );
+};
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/mdc" element={<MDC />} />
-      <Route path="/book-appointment" element={<BookAppointment />} />
-      <Route path="/qualitative-diets" element={<QualitativeDiets />} />
-      <Route path="/:dietName" element={<DynamicDietPage />} />
-      <Route
-        path="/preventive-health-patient-services"
-        element={<PreventiveHealthPatientServices />}
-      />
-      <Route path="/vitrack" element={<Vitrack />} />
-      <Route
-        path="/public-health-interventions"
-        element={<PublicHealthInterventions />}
-      />
-      <Route path="/public-health-academy" element={<PublicHealthAcademy />} />
-      <Route path="/health-and-safety" element={<HealthAndSafety />} />
-      <Route path="/ohs" element={<OHS />} />
-      <Route path="/environmental-health" element={<EnvirementalHealth />} />
-      <Route path="/about-us" element={<AboutUs />} />
-      <Route path="/volunteering" element={<Volunteering />} />
-      <Route path="/auth" element={<Auth />} />
+      {routes.map((route) => {
+        // Set cache headers for the route
+        setCacheHeaders(route.cacheControl);
 
-      <Route path="/users" element={<Users />} />
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/post/:id" element={<Single />} />
-      <Route path="/write" element={<Write />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/share" element={<ShareAroundTheWorld />} />
+        return (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<OptimizedRoute route={route} />}
+          />
+        );
+      })}
     </Routes>
   );
 };
