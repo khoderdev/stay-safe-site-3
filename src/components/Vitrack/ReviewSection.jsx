2117635a4cd131c74b1b8890da0a67bcffeb61ad
oummaldev <oummal.dev@gmail.com>
2025-01-04 15:07:06 +0200

    Some Global Improvements v2

--- src/components/Vitrack/ReviewSection.jsx
+++ src/components/Vitrack/ReviewSection.jsx
@@ -1,57 +1,56 @@
-import React from 'react';
-import { getWarnings } from './conditions';
+import React from "react";
+import { getWarnings } from "./conditions";
 
 const ReviewSection = ({ formData }) => {
-	// Get the list of warnings based on the form data
-	const warnings = getWarnings(formData);
+  // Get the list of warnings based on the form data
+  const warnings = getWarnings(formData);
 
-	return (
-		<div className='p-6 bg-[#000] shadow-md rounded-lg space-y-4'>
-			<div className='bg-black p-4 rounded-lg'>
-				<h2 className='dark:text-white-whites font-semibold text-2xl text-center mb-2'>
-					Health Warnings
-				</h2>
-				{warnings.length > 0 ? (
-					<ul className='pl-6 space-y-2'>
-						{warnings.map((warning, index) => (
-							<li
-								key={index}
-								style={{ color: warning.color || 'red' }}
-								className='font-semibold'
-							>
-								{warning.text}
-							</li>
-						))}
-					</ul>
-				) : (
-					<p className='text-green-400'>
-						All metrics are within normal ranges. No immediate action required.
-					</p>
-				)}
-			</div>
+  return (
+    <div className="p-6 shadow-md rounded-lg space-y-4">
+      <div className="dark:bg-dark p-4 rounded-lg">
+        <h2 className="dark:text-white-whites font-semibold text-2xl text-center mb-2">
+          Health Warnings
+        </h2>
+        {warnings.length > 0 ? (
+          <ul className="pl-6 space-y-2">
+            {warnings.map((warning, index) => (
+              <li
+                key={index}
+                style={{ color: warning.color || "red" }}
+                className="font-semibold"
+              >
+                {warning.text}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-green-400 mt-6">
+            All metrics are within normal ranges. No immediate action required.
+          </p>
+        )}
 
-			{/* Display the submitted form data */}
-			<div className='bg-gray-800 p-4 rounded-lg'>
-				<h2 className='text-white font-semibold text-lg mb-2'>Form Data</h2>
-				<ul className='space-y-2'>
-					{Object.entries(formData).map(([key, value]) => (
-						<li key={key} className='flex justify-between'>
-							<span className='font-semibold text-white-bg2 capitalize'>
-								{key.replace(/([A-Z])/g, ' $1')}
-							</span>
-							<span className='text-white-bg2'>
-								{typeof value === 'object'
-									? JSON.stringify(value, null, 2)
-									: value || 'N/A'}
-							</span>
-						</li>
-					))}
-				</ul>
-			</div>
+        <h2 className="dark:text-white-whites font-semibold text-2xl my-6">
+          Form Data
+        </h2>
+        <ul className="space-y-2">
+          {Object.entries(formData).map(([key, value]) => (
+            <li key={key} className="flex justify-between">
+              <span className="font-semibold dark:text-white-bg2 capitalize">
+                {key.replace(/([A-Z])/g, " $1")}
+              </span>
+              <span className="dark:text-white-bg2">
+                {typeof value === "object"
+                  ? JSON.stringify(value, null, 2)
+                  : value || "N/A"}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-			{/* Display the generated warnings */}
-		</div>
-	);
+      {/* Display the generated warnings */}
+    </div>
+  );
 };
 
 export default ReviewSection;
--- src/components/Vitrack/forms/HealthMetricsForm.jsx
+++ src/components/Vitrack/forms/HealthMetricsForm.jsx
@@ -1,250 +1,3 @@
-// import React from 'react';
-// import { useAtom } from 'jotai';
-// import InputField from '../inputs/InputField';
-// import BloodPressureInput from '../inputs/BloodPressureInput';
-// import { symptomsList } from '../data';
-// import {
-// 	temperatureAtom,
-// 	heartRateAtom,
-// 	respiratoryRateAtom,
-// 	leftHandOxygenAtom,
-// 	rightHandOxygenAtom,
-// 	symptomsAtom,
-// 	painScaleAtom,
-// 	bloodPressureSetsAtom,
-// } from '../../../atoms/store';
-
-// const HealthMetricsForm = () => {
-// 	const [temperature, setTemperature] = useAtom(temperatureAtom);
-// 	const [heartRate, setHeartRate] = useAtom(heartRateAtom);
-// 	const [respiratoryRate, setRespiratoryRate] = useAtom(respiratoryRateAtom);
-// 	const [leftHandOxygen, setLeftHandOxygen] = useAtom(leftHandOxygenAtom);
-// 	const [rightHandOxygen, setRightHandOxygen] = useAtom(rightHandOxygenAtom);
-// 	const [symptoms, setSymptoms] = useAtom(symptomsAtom);
-// 	const [painScale, setPainScale] = useAtom(painScaleAtom);
-// 	const [bloodPressureSets, setBloodPressureSets] = useAtom(
-// 		bloodPressureSetsAtom
-// 	);
-
-// 	const handleChange = (e) => {
-// 		const { name, value } = e.target;
-// 		switch (name) {
-// 			case 'temperature':
-// 				setTemperature(value);
-// 				break;
-// 			case 'heartRate':
-// 				setHeartRate(value);
-// 				break;
-// 			case 'respiratoryRate':
-// 				setRespiratoryRate(value);
-// 				break;
-// 			case 'leftHandOxygen':
-// 				setLeftHandOxygen(value);
-// 				break;
-// 			case 'rightHandOxygen':
-// 				setRightHandOxygen(value);
-// 				break;
-// 			case 'painScale':
-// 				setPainScale(value);
-// 				break;
-// 			default:
-// 				break;
-// 		}
-// 	};
-
-// 	const handleSymptomsChange = (e) => {
-// 		const { value, checked } = e.target;
-// 		setSymptoms((prevSymptoms) =>
-// 			checked
-// 				? [...prevSymptoms, value]
-// 				: prevSymptoms.filter((symptom) => symptom !== value)
-// 		);
-// 	};
-
-// 	const handleDynamicChange = (id, hand, name, value) => {
-// 		setBloodPressureSets((prevSets) =>
-// 			prevSets.map((set) =>
-// 				set.id === id
-// 					? {
-// 							...set,
-// 							[hand]: { ...set[hand], [name]: value },
-// 					  }
-// 					: set
-// 			)
-// 		);
-// 	};
-
-// 	const addBloodPressureSet = () => {
-// 		const newSet = {
-// 			id: bloodPressureSets.length + 1,
-// 			isDefault: false,
-// 			leftHand: { systolic: '', diastolic: '' },
-// 			rightHand: { systolic: '', diastolic: '' },
-// 		};
-// 		setBloodPressureSets([...bloodPressureSets, newSet]);
-// 	};
-
-// 	const removeBloodPressureSet = (id) => {
-// 		setBloodPressureSets(bloodPressureSets.filter((set) => set.id !== id));
-// 	};
-
-// 	return (
-// 		<div className='xsm:p-3 sm:p-7 rounded-lg !bg-white-bg2 dark:!bg-[#000] space-y-6'>
-// 			<InputField
-// 				label='Oral Temperature (°C)'
-// 				name='temperature'
-// 				value={temperature || ''}
-// 				onChange={handleChange}
-// 				type='text'
-// 			/>
-
-// 			<div className='border-2 rounded-lg p-2 space-y-2 border-black'>
-// 				<p className='text-center font-semibold dark:text-white-whites'>
-// 					Blood Pressure
-// 				</p>
-// 				{/* Render dynamic sets of blood pressure inputs */}
-// 				{bloodPressureSets.map((set) => (
-// 					<div
-// 						key={set.id}
-// 						className='space-y-4 relative border border-black p-4 rounded'
-// 					>
-// 						{/* Remove Button */}
-// 						{!set.isDefault && (
-// 							<button
-// 								type='button'
-// 								onClick={() => removeBloodPressureSet(set.id)}
-// 								className='absolute top-2 right-2 text-red-600 hover:text-red-800 text-2xl font-bold'
-// 							>
-// 								-
-// 							</button>
-// 						)}
-
-// 						{/* Blood Pressure Inputs */}
-// 						<div className='xsm:grid xsm:grid-cols-1 sm:flex-col sm:flex sm:space-x-0 sm:space-y-3 sm:items-center md:flex md:flex-row md:space-x-8 md:space-y-0 md:items-center md:justify-center'>
-// 							<BloodPressureInput
-// 								hand='Left'
-// 								systolic={set.leftHand.systolic}
-// 								diastolic={set.leftHand.diastolic}
-// 								onChange={(e) =>
-// 									handleDynamicChange(
-// 										set.id,
-// 										'leftHand',
-// 										e.target.name,
-// 										e.target.value
-// 									)
-// 								}
-// 							/>
-// 							<BloodPressureInput
-// 								hand='Right'
-// 								systolic={set.rightHand.systolic}
-// 								diastolic={set.rightHand.diastolic}
-// 								onChange={(e) =>
-// 									handleDynamicChange(
-// 										set.id,
-// 										'rightHand',
-// 										e.target.name,
-// 										e.target.value
-// 									)
-// 								}
-// 							/>
-// 						</div>
-// 					</div>
-// 				))}
-
-// 				{/* Button to add a new blood pressure set */}
-// 				<div className='flex space-x-4'>
-// 					<button
-// 						type='button'
-// 						onClick={addBloodPressureSet}
-// 						className='flex items-center font-bold text-3xl text-blue-600 hover:text-blue-800'
-// 					>
-// 						+
-// 					</button>
-// 				</div>
-// 			</div>
-
-// 			{/* Other Inputs */}
-// 			<InputField
-// 				label='Heart Rate (bpm)'
-// 				name='heartRate'
-// 				value={heartRate || ''}
-// 				onChange={handleChange}
-// 				type='text'
-// 			/>
-// 			<InputField
-// 				label='Respiratory Rate (breaths/min)'
-// 				name='respiratoryRate'
-// 				value={respiratoryRate || ''}
-// 				onChange={handleChange}
-// 				type='text'
-// 			/>
-// 			<div className='grid grid-cols-2 gap-x-14'>
-// 				{/* Left Hand Oxygen */}
-// 				<InputField
-// 					label='Left Hand Oxygen Saturation (%)'
-// 					name='leftHandOxygen'
-// 					value={leftHandOxygen || ''}
-// 					onChange={handleChange}
-// 					type='text'
-// 					max={100}
-// 				/>
-
-// 				{/* Right Hand Oxygen */}
-// 				<InputField
-// 					label='Right Hand Oxygen Saturation (%)'
-// 					name='rightHandOxygen'
-// 					value={rightHandOxygen || ''}
-// 					onChange={handleChange}
-// 					type='text'
-// 					max={100}
-// 				/>
-
-// 				{/* Symptoms */}
-// 				<div className='col-span-full mt-6'>
-// 					<h2 className='text-lg font-semibold dark:text-white-bg2'>
-// 						Symptoms
-// 					</h2>
-// 					<div className='grid grid-cols-1 sm:grid-cols-2 gap-2'>
-// 						{symptomsList.map((symptom, index) => (
-// 							<label key={index} className='flex items-center space-x-2'>
-// 								<input
-// 									type='checkbox'
-// 									value={symptom || ''}
-// 									onChange={handleSymptomsChange}
-// 									checked={symptoms.includes(symptom)}
-// 									className='form-checkbox'
-// 								/>
-// 								<span className='dark:text-white-bg2'>{symptom}</span>
-// 							</label>
-// 						))}
-// 					</div>
-// 				</div>
-
-// 				{/* Pain Scale */}
-// 				<div className='col-span-full mt-6'>
-// 					<label className='block text-sm dark:text-white-bg2'>
-// 						Pain Scale
-// 					</label>
-// 					<input
-// 						type='range'
-// 						name='painScale'
-// 						min='0'
-// 						max='10'
-// 						value={painScale || 0}
-// 						onChange={handleChange}
-// 						className='mt-1 w-full cursor-pointer'
-// 					/>
-// 					<span className='block text-center dark:text-white-bg2'>
-// 						{painScale}/10
-// 					</span>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default HealthMetricsForm;
-
 import React from 'react';
 import { useAtom } from 'jotai';
 import InputField from '../inputs/InputField';
@@ -260,7 +13,7 @@ import {
 	painScaleAtom,
 	bloodPressureSetsAtom,
 } from '../../../atoms/store';
-import { getWarnings } from '../conditions'; // Assuming this is where your getWarnings logic resides
+import { getWarnings } from '../conditions';
 
 const HealthMetricsForm = () => {
 	const [temperature, setTemperature] = useAtom(temperatureAtom);
@@ -386,16 +139,23 @@ const HealthMetricsForm = () => {
 				)}
 			</div>
 			{/* Blood Pressure Inputs */}
-			<div className='border-2 rounded-lg p-2 space-y-2 border-black'>
+			<div className='border-2 rounded-lg p-2 space-y-2 dark:border-black'>
 				<p className='text-center font-semibold dark:text-white-whites'>
 					Blood Pressure
 				</p>
 				{bloodPressureSets.map((set) => (
 					<div
 						key={set.id}
-						className='space-y-4 relative border border-black p-4 rounded'
+						className='space-y-4 relative border dark:border-black p-4 rounded'
 					>
 						{/* Blood Pressure Inputs */}
+						<button
+							type='button'
+							className='absolute font-bold text-3xl hover:scale-125 duration-200 top-2 right-2 text-red-500'
+							onClick={() => removeBloodPressureSet(set.id)}
+						>
+							- 
+						</button>
 						<div className='xsm:grid xsm:grid-cols-1 sm:flex-col sm:flex sm:space-x-0 sm:space-y-3 sm:items-center md:flex md:flex-row md:space-x-8 md:space-y-0 md:items-center md:justify-center'>
 							<BloodPressureInput
 								hand='Left'
@@ -430,7 +190,7 @@ const HealthMetricsForm = () => {
 					<button
 						type='button'
 						onClick={addBloodPressureSet}
-						className='flex items-center font-bold text-3xl text-blue-600 hover:text-blue-800'
+						className='flex items-center font-bold text-3xl text-pink hover:scale-125 duration-200'
 					>
 						+
 					</button>
